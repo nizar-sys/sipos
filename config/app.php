5098ac42c161bf2b192ee42fe460f5d62dce76c4
nizar-sys <raspbery132@gmail.com>
2021-11-17 19:54:23 +0700

    Sistem Point Of Sales v0.6

--- config/app.php
+++ config/app.php
@@ -54,7 +54,7 @@ return [
 
     'url' => env('APP_URL', 'https://sipos.herokuapp.com'),
 
-    'asset_url' => env('ASSET_URL', null),
+    'asset_url' => env('ASSET_URL', 'https://sipos.herokuapp.com'),
 
     /*
     |--------------------------------------------------------------------------
